443a4d66c086dd9a9219eb84fc5cf2b63eeb5a27
Rune_Coal <andersonsousac19@gmail.com>
2026-03-19 11:43:47 -0300

    Arrays bidimensionais

--- Atividade_03/cinema.js
+++ Atividade_03/cinema.js
@@ -6,3 +6,5 @@ let assentos = [
 
 let cadeiraMeio = assentos [1][1]
 console.log("A cadeira do meio está " + cadeiraMeio);
+assentos[0][0] = "O";
+console.log("O assento em [0][0] está " + assentos[0][0])
