be3a957313db99b486a291d64ac7db1da628b647
Rune_Coal <andersonsousac19@gmail.com>
2026-03-19 11:27:20 -0300

    Assento de Cinemas

---
+++ Atividade_03/cinema.js
@@ -0,0 +1,8 @@
+let assentos = [
+      ["L", "L", "O"],
+      ["L", "O", "L"],
+      ["O", "L", "L"]
+];
+
+let cadeiraMeio = assentos [1][1]
+console.log("A cadeira do meio está " + cadeiraMeio);
