76e54bdbdf4b0bb911cd019178abf917aa07c99b
Anderson de Sousa Carvalho <>
2026-05-26 12:16:35 -0300

    RPG ataqye e mana

--- atividade_rpg/javascript.js
+++ atividade_rpg/javascript.js
@@ -6,6 +6,33 @@ class Personagem {
         this.mana = mana;
         this.energia = energia;
     }
+    hero_atacar(alvo, habilidade) {
+        console.log (this.mana)
+        if (this.mana >= habilidade.custo
+            && this.energia >= habilidade.energia) {
+                
+                alvo.hp =  alvo.hp - habilidade.dano;
+                // Debitar Mana
+                if (habilidade.custo > 0) {
+                    this.mana -= habilidade.custo;
+                    this.energia += 50;
+                }
+                // Debitar Energia
+                if (habilidade.energia > 0) {
+                    this.energia = 0;
+                }
+            } else {
+                return "Sem mana ou energia.";
+            }
+    }
+    boss_atacar(alvo) {
+        if(this.energia >= 100) {
+            alvo.hp -= 15;
+            this.energia = 0;
+        } else {
+            this.energia +=50;
+        }
+    }
 }
 class Habilidade {
     constructor(id, nome, dano, custo, energia) {
@@ -33,13 +60,24 @@ document.getElementById("titulo-boss").textContent = boss.titulo;
 
 let containerBtn = document.getElementById("controles");
 let listaHabilidades =      [
-    new Habilidade(1, "Ataque⚔️", 4, 0, 0),
-    new Habilidade(2, "Skill✨", 8, 10, 0),
-    new Habilidade(3, "Ultimate💥", 15, 0, 100)
+    new Habilidade(1, "Ataque⚔️", 5, 0, 0),
+    new Habilidade(2, "Skill✨", 10, 15, 0),
+    new Habilidade(3, "Ultimate💥", 20, 0, 100)
 ];
 listaHabilidades.forEach(hab => {
     let btn = document.createElement("button");//<button>
     btn.innerText = hab.nome;
+    btn.classList.add("btn","btn-light");
     containerBtn.appendChild(btn);
+    btn.onclick = () => {
+        console.log(boss.hp)
+        hero.hero_atacar(boss, hab);
+        atualizarTela();
+    }
 
-})
+}) ;
+const atualizarTela = () => {
+    document.getElementById("hp-boss").value = boss.hp;
+    document.getElementById("mp-hero").value = hero.mana;
+    document.getElementById("en-hero").value = hero.energia;
+}
